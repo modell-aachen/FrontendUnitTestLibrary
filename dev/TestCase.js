500252003860c79f5069054fed25cd904f215da9
Andre Tebart <tebart@modell-aachen.de>
2017-07-14 15:55:08 +0200

    Changes to comply to the new way the VueJsPlugin works.
    Ticket: none
    
    Release-Note: none

--- dev/TestCase.js
+++ dev/TestCase.js
@@ -1,10 +1,18 @@
-import 'es6-promise/auto'
+import 'es6-object-assign/auto'; //Needed for IE
+import 'es6-promise/auto'; //Needed for IE
+
 import Vue from 'vue';
 import Vuex from 'vuex';
-import './mocks/VueJSPlugin.js';
+import MAVueJsPluginMock from './mocks/MAVueJsPlugin.js';
+
+Vue.use(Vuex);
+
+let store = new Vuex.Store();
+Vue.use(MAVueJsPluginMock, {store});
+
+window.Vue = Vue;
 
 export default {
-  vuexStore: new Vuex.Store({}),
   createVueComponent(componentDefinition, constructionOptions) {
     const Ctor = Vue.extend(componentDefinition);
     return new Ctor(constructionOptions);
---
+++ dev/mocks/MAVueJsPlugin.js
@@ -0,0 +1,42 @@
+import VueSelectMock from './VueSelect.vue';
+import VuePaginationMock from './VuePagination.vue';
+import FoswikiMock from './foswiki.js';
+import moment from 'moment';
+import $ from 'jquery';
+
+
+let MAVueJsPluginMock = {
+	install(Vue, options) {
+		Vue.component('vue-select', VueSelectMock);
+		Vue.component('vue-pagination', VuePaginationMock);
+
+		//Global functions
+		Vue.registerStoreModule = (name, module) => {
+			options.store.registerModule(name, module);
+		};
+
+		Vue.onDocumentReady = (fn) => {
+			$(fn);
+		};
+
+		Vue.instantiateEach = (selector, options) => {
+			$(selector).each((i, element) => {
+				let instanceOptions = Object.assign({}, options);
+				instanceOptions.el = element;
+				new Vue(instanceOptions);
+			});
+		}
+
+		Vue.getConfigById = (id) => {
+			return JSON.parse($('.' + id).html());
+		};
+
+		Vue.prototype.$store = options.store;
+		Vue.prototype.$foswiki = FoswikiMock;
+		Vue.prototype.$moment = moment;
+		Vue.prototype.$lang = "en";
+		Vue.prototype.$ajax = $.ajax;
+	}
+}
+
+export default MAVueJsPluginMock;
